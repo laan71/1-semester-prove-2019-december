ba069a4638e57ef96fd69f3c7bca9cc2385fe423
laan71 <lail5198@edu.easj.dk>
2019-12-20 14:18:47 +0100

    Filer oprettet

---
+++ src/Quiz/Quiz.java
@@ -0,0 +1,8 @@
+package Quiz;
+
+public class Quiz {
+
+    /* Designe og udvikl 3 nye java-klasser til en quiz-applikation: Sporgsmaal, Svar og Quiz.
+       Lav en ny pakke, som hedder quiz.
+     */
+}
