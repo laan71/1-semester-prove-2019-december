5aac2aa93a8975ec4d6167d6979c4013235d4879
laan71 <lail5198@edu.easj.dk>
2019-12-20 09:57:09 +0100

    Opgave 1 Hello World

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,19 @@
 
 public class Main {
 
-    // TODO Din kode her
+     /* Skriv en "Hello world!"-applikation i Java, klassenavnet skal være Main.
+    Lav en hello(String navn) metode i klassen. Metoden skal returnere teksten "Hello András!", når man kalder den som: hello("András")
+    Hvis man kalder metoden med en tom tekststreng hello(""), så skal den returnere "Hello World!” */
 
-    public String hello(String navn) {
-        return "";
+    public static void main(String[] args) {
+        String greeting = hello("András");
+        System.out.println(greeting);
+    }
+    public static String hello(String name) {
+        if (name.isEmpty()) {
+            name = "World!";
+        }
+        return ("Hello " + name);
     }
 
 }
