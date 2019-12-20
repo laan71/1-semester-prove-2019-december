e509f0d71013d94db7443bec3b2dae52cc8a6efa
laan71 <lail5198@edu.easj.dk>
2019-12-20 13:43:29 +0100

    Opgave 4 Loops og arrays

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+import static java.util.Collections.reverse;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -6,6 +12,12 @@
  */
 
 public class Loops {
+
+    /*Skriv en metode, der hedder waitingFor0(). Metoden skal blive ved med at indlæse tal fra brugeren så længe, tallet ikke er 0.
+    Brug en while-loop og Scanner til indlæsning af tal.
+    Skriv en metode, der hedder printBrugereArr(). Metoden skal vise alle brugere fra brugere[] vha. et for loop.
+    Skriv en metode, der hedder printBrugereReverseArr(). Metoden skal vise alle brugere fra brugere[] i omvendt rækkefølge.
+     */
 
     private String brugere[] = {"Anders", "Birgit", "Carsten", "Dennis", "Esben"};
 
@@ -23,25 +35,43 @@ public class Loops {
     }
 
     /**
-     * Viser en liste med brugere i konsollen
+     * Viser en liste med brugere i konsollen vha en for løkke
      */
     public void printBrugereArr() {
 
-        // TODO Din kode her
+        for (String user: brugere) {
+            System.out.println(user);
+        }
     }
 
     /**
-     * Viser en liste med brugere i konsollen i omvendt rækkefølge
+     * Viser en liste med brugere i konsollen i omvendt rækkefølge vha en for løkke
      */
     public void printBrugereReverseArr() {
 
-        // TODO Din kode her
+        for (int count = brugere.length - 1; count >= 0; count--) {
+            System.out.println(brugere[count]);
+        }
+
 
     }
 
+    // Indlæs tal så længe tallet ikke er 0 vha en while løkke
+    
     public void waitingFor0() {
 
-        // TODO Din kode her
+        int i = 25;
+        while ( i != 0 ) {
+            System.out.println("Venter på 0!");
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Skriv et tal... ");
+            i = scanner.nextInt();
+
+            if ( i == 0 ) {
+                System.out.println("Du har tastet 0. Så er det slut!");
+            }
+        }
  
     }
 }
